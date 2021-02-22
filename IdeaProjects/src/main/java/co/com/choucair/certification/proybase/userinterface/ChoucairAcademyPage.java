6bd245b40b17ed7aabf48f34d049ce7a6564b51a
david.cruz <dfcc15@hotmail.com>
2021-02-21 20:00:57 -0500

    Corrección sugerida de parte de choucair testing

--- IdeaProjects/src/main/java/co/com/choucair/certification/proybase/userinterface/ChoucairAcademyPage.java
+++ IdeaProjects/src/main/java/co/com/choucair/certification/proybase/userinterface/ChoucairAcademyPage.java
@@ -5,5 +5,8 @@ import net.thucidydes.core.annotations.DefaultUrl;
 
 
 @DefaultUrl("https://operacion.choucairtesting.com/academy/login/index.php")
-public class ChoucairAcademyPage {
+public class ChoucairAcademyPage extends PageObject{
+
+    public static final Target LOGIN_BUTTON = Target.the("button that shows us the form to login").located(By.xpath("//div[@class='d-none d-lg-block']//strong[contains(text(),'Ingresar')]"));
+
 }
--- IdeaProjects/src/main/java/co/com/choucair/certification/proybase/userinterface/ChoucairLoginPage.java
+++ IdeaProjects/src/main/java/co/com/choucair/certification/proybase/userinterface/ChoucairLoginPage.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.By;
 
 
 
-public class ChoucairLoginPage extends PageObject {
+public class ChoucairLoginPage {
 
 public static final Target LOGIN_BUTTON = Target.the("button that shows us the form to login")
         .located(By.xpath("//div[@class='d-none d-lg-block']//strong[contains(text(),'Ingresar')]"));
